70ff948cb6540c52d61afd4eaed9bcd3e1816904
Anatoliy Cherevach <anatoliycherevach@gmail.com>
2021-05-03 03:44:26 +0300

    Lab 3.2

--- App.js
+++ App.js
@@ -1,61 +1,56 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
-import Genetic from './src/Genetic'
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import Perceptron from './src/Perceptron';
 
 export default function App() {
-  const [a, setA] = useState(null);
-  const [b, setB] = useState(null);
-  const [c, setC] = useState(null);
-  const [d, setD] = useState(null);
-  const [y, setY] = useState(null);
-  const [result, setResult] = useState('[]');
+  const [learningRate, setLearningRate] = useState(0.001);
+  const [deadline, setDeadline] = useState(100);
+  const [result, setResult] = useState();
+  const [time, setTime] = useState();
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <TextInput style={styles.expression}
-          onChangeText={setA}
-          value={a}
-          placeholder="a"
-          keyboardType="numeric"
-        />
-        <Text style={styles.expression}>{'*x1 + '}</Text>
-        <TextInput style={styles.expression}
-          onChangeText={setB}
-          value={b}
-          placeholder="b"
-          keyboardType="numeric"
-        />
-        <Text style={styles.expression}>{'*x2 + '}</Text>
-        <TextInput style={styles.expression}
-          onChangeText={setC}
-          value={c}
-          placeholder="c"
-          keyboardType="numeric"
-        />
-        <Text style={styles.expression}>{'*x3 + '}</Text>
-        <TextInput style={styles.expression}
-          onChangeText={setD}
-          value={d}
-          placeholder="d"
-          keyboardType="numeric"
-        />
-        <Text style={styles.expression}>{'*x4 = '}</Text>
-        <TextInput style={styles.expression}
-          onChangeText={setY}
-          value={y}
-          placeholder="y"
-          keyboardType="numeric"
-        />
-      </View>
+    <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+
+      <RNPickerSelect
+        style={pickerSelectStyles}
+        onValueChange={(value) => setLearningRate(value)}
+        placeholder={{ label: 'Оберіть швидкість навчання', value: null }}
+        items={[
+          { label: '0.001', value: 0.001 },
+          { label: '0.01', value: 0.01 },
+          { label: '0.05', value: 0.05 },
+          { label: '0.1', value: 0.1 },
+          { label: '0.2', value: 0.2 },
+          { label: '0.3', value: 0.3 },
+        ]}
+      />
+      <RNPickerSelect
+        style={pickerSelectStyles}
+        onValueChange={(value) => setDeadline(value)}
+        placeholder={{ label: 'Оберіть дедлайн', value: null }}
+        items={[
+          { label: '100', value: 100 },
+          { label: '200', value: 200 },
+          { label: '500', value: 500 },
+          { label: '1000', value: 1000 },
+
+        ]}
+      />
       <Text style={styles.result}>
-        {`[x1, x2, x3, x4] = ${result}`}
+        {[result, time]}
       </Text>
       <View style={styles.btn}>
         <Button
-          title="Calculate"
+          title="Learn"
           color="#fff"
-          onPress={() => setResult(new Genetic([a, b, c, d], y).solve())}
+          onPress={() => {
+            const p = new Perceptron({ threshold: 4, learningRate })
+            let start = performance.now();
+            setResult(`${p.learn([[0, 6], [3, 3], [1, 5], [2, 4]], deadline)}`)
+            let end = performance.now();
+            setTime(`\nTime= ${end - start}`)
+          }}
         />
       </View>
 
@@ -65,34 +60,37 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
-  container: {
-    width: '90%',
-    top: 200,
-    flexDirection: 'row',
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  expression: {
-    fontSize: 25
-  },
   result: {
+    top: 100,
+    lineHeight: 30,
     alignSelf: 'center',
-    top: 269,
-    fontSize: 25
-  },
-  time: {
-    alignSelf: 'center',
-    top: 320,
-    fontSize: 22
+    fontSize: 16,
   },
   btn: {
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
-    top: 400,
+    top: 200,
     height: 50,
     width: 150,
     backgroundColor: 'black',
+  },
+});
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    top: 20,
+    width: '90%',
+    alignSelf: 'center',
+    marginVertical: 10,
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 6,
+    color: 'black',
+  },
+  inputAndroid: {
+
   },
 });
